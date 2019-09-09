ae8e87338a004c2d295920033d1a368264c59e81
Tommy Kovathana <tkovathana@yahoo.com>
2019-09-09 06:47:27 -0500

    Added new intent

--- app/src/main/java/com/example/madlibs/MainActivity.java
+++ app/src/main/java/com/example/madlibs/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     String cool;
@@ -10,6 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendInfo(View v)
+    {
+        EditText adj = (EditText) findViewById(R.id.adjEt);
+        EditText adv = (EditText) findViewById(R.id.adverbEt);
+        EditText noun = (EditText) findViewById(R.id.nounEt);
+        EditText verb = (EditText) findViewById(R.id.verbEt);
+        EditText place = (EditText) findViewById(R.id.placeEt);
+        EditText name = (EditText) findViewById(R.id.nameEt);
+
+        String adjString = adj.getText().toString();
+        String advString = adv.getText().toString();
+        String nounString = noun.getText().toString();
+        String verbString = verb.getText().toString();
+        String placeString = place.getText().toString();
+        String nameString = name.getText().toString();
+
+        Intent intent = new Intent(this, MainActivity2.class);
+
+        intent.putExtra(MainActivity2.ADJ, adjString);
+        intent.putExtra(MainActivity2.ADV, advString);
+        intent.putExtra(MainActivity2.NOUN, nounString);
+        intent.putExtra(MainActivity2.VERB, verbString);
+        intent.putExtra(MainActivity2.PLACE, placeString);
+        intent.putExtra(MainActivity2.NAME, nameString);
+
+        startActivity(intent);
+
     }
 
 }
---
+++ app/src/main/java/com/example/madlibs/MainActivity2.java
@@ -0,0 +1,37 @@
+package com.example.madlibs;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String adj = intent.getStringExtra(ADJ);
+        String adv = intent.getStringExtra(ADV);
+        String noun = intent.getStringExtra(NOUN);
+        String verb = intent.getStringExtra(VERB);
+        String place = intent.getStringExtra(PLACE);
+        String name = intent.getStringExtra(NAME);
+
+        String strDisplay = "There once was a person named " + name + " and they were " + adj + ". Everyone laughed at them for " + adv + verb +
+                            "everyday. " + name + "only liked to be at " + place + " because it was the only place that had a " + noun + ".";
+
+        TextView str = (TextView) findViewById(R.id.info);
+        str.setText(strDisplay);
+
+    }
+
+    public static final String ADJ = "adj";
+    public static final String ADV = "adj";
+    public static final String NOUN = "adj";
+    public static final String VERB = "adj";
+    public static final String PLACE = "adj";
+    public static final String NAME = "adj";
+}
